483bd9050b606893b6d063364156a6bbb1873a59
Tim Sharpe <tim@github.com>
2012-10-04 11:45:11 -0700

    specs for git::config::local defined type

---
+++ spec/defines/git__config__local_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe 'git::config::local' do
+  let(:title) { '/tmp/test' }
+  let(:params) do
+    {
+      :key    => 'user.name',
+      :value  => 'Hugh Bot',
+      :ensure => 'present',
+    }
+  end
+
+  it do
+    namevar = "set #{params[:key]} to #{params[:value]} in #{title}"
+    should contain_ini_setting(namevar).with({
+      :ensure  => params[:ensure],
+      :path    => "#{title}/.git/config",
+      :section => 'user',
+      :setting => 'name',
+      :value   => params[:value],
+    })
+  end
+end
