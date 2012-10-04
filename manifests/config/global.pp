b81c24db11915eb63d5e35e7c3c743e179265da3
Tim Sharpe <tim@github.com>
2012-10-04 11:48:09 -0700

    tomdoc git::config::global

--- manifests/config/global.pp
+++ manifests/config/global.pp
@@ -1,12 +1,26 @@
+# Public: Set a global git configuration value.
+#
+# namevar - The String name of the configuration option.
+# value   - The String value of the configuration option.
+#
+# Examples
+#
+#   git::config::global { 'user.name':
+#     value => 'Hugh Bot',
+#   }
+#
+#   git::config::global { 'user.email':
+#     value => 'test@example.com',
+#   }
 define git::config::global($value) {
   $split_key = split($name, '\.')
   $path = "/Users/${::luser}/.gitconfig"
 
   ini_setting { "set ${name} to ${value} in ${path}":
+    ensure  => present,
     path    => $path,
     section => $split_key[0],
     setting => $split_key[1],
     value   => $value,
-    ensure  => present
   }
 }
