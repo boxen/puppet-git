3c78b14d367ab8c2d81b9813861ef49d66f69602
Tim Sharpe <tim@github.com>
2012-10-04 11:52:31 -0700

    tomdoc git::config::local defined type

--- manifests/config/local.pp
+++ manifests/config/local.pp
@@ -1,3 +1,20 @@
+# Public: Set a git configuration option for a specific repository.
+#
+# namevar - The String path to the git repository.
+# key     - The String name of the configuration option.
+# value   - The String value of the configuration option.
+# ensure  - The desired state of the resource as a String.  Valid values are
+#           'present' and 'absent' (default: 'present').
+#
+# Examples
+#
+#   git::config::local { '/tmp/foo':
+#     key   => 'user.name',
+#     value => 'Hugh Bot',
+#   }
+#
+# FIXME - Using namevar like this means you can only set 1 option per repo,
+#         not exactly ideal.
 define git::config::local($key, $value, $ensure = present) {
   $split_key = split($key, '\.')
 
