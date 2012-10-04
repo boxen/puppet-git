4eac121e2078cb73f7b8e48d149d352e5023c96f
Tim Sharpe <tim@github.com>
2012-10-04 11:44:59 -0700

    specs for git::config::global defined type

---
+++ spec/defines/git__config__global_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe 'git::config::global' do
+  let(:title) { 'user.name' }
+  let(:params) { {:value => 'Hugh Bot'} }
+  let(:facts) { {:luser => 'hubot'} }
+
+  let(:path) { '/Users/hubot/.gitconfig' }
+
+  it do
+    should contain_ini_setting("set #{title} to #{params[:value]} in #{path}").with({
+      :path    => path,
+      :section => 'user',
+      :setting => 'name',
+      :value   => params[:value],
+      :ensure  => 'present',
+    })
+  end
+end
