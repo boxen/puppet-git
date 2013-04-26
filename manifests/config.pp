06572c78b9c842250aae4200538faaa1680219d9
Matthew Rothenberg <mrothenberg@gmail.com>
2013-04-26 17:43:04 -0400

    bump git to v1.8.2.1

--- manifests/config.pp
+++ manifests/config.pp
@@ -5,7 +5,7 @@ class git::config {
   $credentialhelper = "${boxen::config::repodir}/script/boxen-git-credential"
   $global_credentialhelper = "${boxen::config::home}/bin/boxen-git-credential"
 
-  $version = "1.8.2-boxen1"
+  $version = "1.8.2.1-boxen1"
 
 
   anchor { [
